1c363a3941654396fa84623dc4ad47ad3769438e
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2024-03-25 23:33:00 +0600

    finished the blogs page

--- src/main.jsx
+++ src/main.jsx
@@ -2,36 +2,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import Blogs from './pages/Blogs.jsx'
-import BookMarks from './pages/BookMarks.jsx'
-import Mainlayout from './layouts/Mainlayout.jsx'
-
-const router=createBrowserRouter([
-
-{
-  path:'/',
-  element:<Mainlayout></Mainlayout>,
-  children:[
-    {
-      path:'/',
-      element:<Home></Home>
-    },
-    {
-      path:'/blogs',
-      element:<Blogs></Blogs>
-    },
-    {
-      path:'/bookmarks',
-      element:<BookMarks></BookMarks>
-    }
-  ]
-},
+import { RouterProvider } from 'react-router-dom'
+import {router} from './routes/Routes.jsx'
 
 
 
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
